f1f3d1dbd5ea87032b32ad286e77e7b9f46a4619
JR <jean-romain.roussel.1@ulaval.ca>
2017-04-18 15:36:23 -0400

    Use rgl feature to render plot

--- R/ui_treeviewer.r
+++ R/ui_treeviewer.r
@@ -1,20 +1,23 @@
 ui_treeviewer = function(.las)
 {
   ui = shiny::shinyUI(shiny::pageWithSidebar(
+
     shiny::headerPanel("Tree viewer"),
 
     shiny::sidebarPanel(
       shiny::sliderInput("tree", "Tree:", min = 0, max = max(.las$treeID), value = 0, step = 1)
     ),
 
-    shiny::mainPanel(shinyRGL::webGLOutput("myWebGL"))
+    shiny::mainPanel(rgl::rglwidgetOutput("myWebGL"))
   ))
 
   server = shiny::shinyServer(function(input, output) {
 
-    output$myWebGL <- shinyRGL::renderWebGL({
+    output$myWebGL <- rgl::renderRglwidget({
     tree = suppressWarnings(lasfilter(.las, treeID == input$tree))
+    try(rgl.close())
     plot(tree, add = TRUE)
+    rglwidget()
     })
   })
 
